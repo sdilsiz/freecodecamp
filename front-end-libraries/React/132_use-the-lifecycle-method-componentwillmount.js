694543efd0ee4ec6f26b35c439717bcb868726d1
sdilsiz <dilsizsalih@gmail.com>
2020-10-09 23:43:00 +0200

    React component will/did mount

---
+++ front-end-libraries/React/132_use-the-lifecycle-method-componentwillmount.js
@@ -0,0 +1,14 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // Change code below this line
+  console.log("componentWillMount")
+      // Change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
+  
