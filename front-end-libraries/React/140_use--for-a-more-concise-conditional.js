2d223d2089f384d33e4ef5c1946f40168cecfe56
sdilsiz <dilsizsalih@gmail.com>
2020-10-12 19:51:38 +0200

    start react_redux

---
+++ front-end-libraries/React/140_use--for-a-more-concise-conditional.js
@@ -0,0 +1,34 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+      this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+      this.setState(state => ({
+        display: !state.display
+      }));
+    }
+    render() {
+      // Change code below this line
+      if(<p>markup</p>&&this.state.display){
+      return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+           <h1>Displayed!</h1>
+         </div>
+      );}
+      else {
+   return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+          
+         </div>
+      );
+  
+      }
+    }
+  };
+  
---
+++ front-end-libraries/react-and-redux/102_manage-state-locally-first.js
@@ -0,0 +1,22 @@
+class DisplayMessages extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        messages: []
+      }
+    }
+    // Add handleChange() and submitMessage() methods here
+  
+    render() {
+      return (
+        <div>
+          <h2>Type in a new Message:</h2>
+          { /* Render an input, button, and ul below this line */ }
+  
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+  
