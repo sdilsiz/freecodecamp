631ed42a38f0ad75a8a8c465cf4ba7563518ff7e
sdilsiz <dilsizsalih@gmail.com>
2020-10-12 15:00:28 +0200

    react learn

---
+++ front-end-libraries/React/137_add-inline-styles-in-react.js
@@ -0,0 +1,11 @@
+// Change code above this line
+class Colorful extends React.Component {
+    render() {
+      // Change code below this line
+      return (
+        <div style={{color: "yellow", fontSize: 24}}>Style Me!</div>
+      );
+      // Change code above this line
+    }
+  };
+  
