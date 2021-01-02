8331fa0e8c27d28449f98ae93c3bc58cd97ac3f8
sdilsiz <dilsizsalih@gmail.com>
2021-01-02 01:12:26 +0100

    removed pyc files

--- scientific-computing-with-python/time-calculator/main.py
+++ scientific-computing-with-python/time-calculator/main.py
@@ -1,11 +1,12 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import add_time
+#import add_time
 from unittest import main
+import time_calculator
 
 
 #print(add_time("11:06 PM", "2:02"))
 
-add_time("11:55 AM", "3:12")
+time_calculator.add_time("11:55 AM", "3:12")
 
 
 # Run unit tests automatically
